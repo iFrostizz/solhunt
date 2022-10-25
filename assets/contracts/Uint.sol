43b818d9a458ea346d6e929d2965b737a69e2c22
franfran <fgg67@hotmail.fr>
2022-10-26 00:29:32 +0200

    clean

--- assets/contracts/Uint.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.8.0;
-
-contract Uint {
-    uint256 unint1;
-    uint unint2;
-}
--- assets/contracts/Void.t.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity ^0.8.0;
-
-contract VoidTest {
-    function setUp() public {}
-}
